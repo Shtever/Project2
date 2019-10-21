cc5f34b8a2de2dd671eab194e073f119e9712138
William Lukas <iggyboy456@gmail.com>
2019-10-21 15:52:34 -0500

    almost

--- public/js/homeowner.js
+++ public/js/homeowner.js
@@ -1,40 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // initializing variables
-  var newCandy = $("#newCandy").val();
-  var existingCandy = $("#candyInput").val();
-  var candyAdd;
-  var newNeighborhood = $("#newNeighborhood").val();
-  var existingNeighborhood = $("#neighborhoodInput").val();
-  var neighborhoodAdd;
-
-  if (!newCandy) {
-    candyAdd = existingCandy;
-    console.log(candyAdd);
-  } else {
-    candyAdd = newCandy;
-    console.log(candyAdd);
-  }
-
-  if (!newNeighborhood) {
-    neighborhoodAdd = existingNeighborhood;
-    console.log(neighborhoodAdd);
-  } else {
-    neighborhoodAdd = newNeighborhood;
-    console.log(neighborhoodAdd);
-  }
-
-  $("#sub-btn").on("click", add());
+  $("#sub-btn").on("click", function(event){
+    event.preventDefault()
+    add(event);
+  });
 
   function add(event) {
+    var newCandy = $("#newCandy").text();
+    var existingCandy = $("#candyInput").text();
+    var candyAdd;
+    var newNeighborhood = $("#newNeighborhood").text();
+    var existingNeighborhood = $("#neighborhoodInput").text();
+    var neighborhoodAdd;
+
+    console.log(newCandy);
+    console.log(newNeighborhood);
     event.preventDefault;
+    if (!newCandy) {
+      candyAdd = existingCandy;
+      console.log(candyAdd);
+    } else {
+      candyAdd = newCandy;
+      console.log(candyAdd);
+    }
+
+    if (!newNeighborhood) {
+      neighborhoodAdd = existingNeighborhood;
+      console.log(neighborhoodAdd);
+    } else {
+      neighborhoodAdd = newNeighborhood;
+      console.log(neighborhoodAdd);
+    }
 
     newPost = {
       candy: candyAdd,
       neighborhood: neighborhoodAdd
     };
     console.log(newPost);
-    $.post("/api/treats", newPost, function() {
-      window.location.href = "/homeowner";
+    $.post("/api/treats", newPost, function () {
+      // window.location.href = "/homeowner";
+      console.log("posted!");
     });
   }
 });
