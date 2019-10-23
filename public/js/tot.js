633e8ef464f9cbf33f3a0637f836167aa909f22c
Larkin <larkiningram@gmail.com>
2019-10-23 11:37:50 -0500

    fixed issue where site confuses neighborhoods with more than one candy

--- public/js/tot.js
+++ public/js/tot.js
@@ -1,46 +1,46 @@
-$(document).ready(function() {
-  function populateTreats () {
-    var treats = [];
-    $.get("/api/treats", function (data) {
-      for (i in data) {
-        if (treats.includes(data[i].candy) !== true) {
-          $("#candyInput").append(
-            $(
-              "<option value='" +
-                data[i].candy +
-                "'>" +
-                data[i].candy +
-                "</option>"
-            )
-          );
-          treats.push(data[i].candy);
-        }
-      }
-    });
-  }
-  populateTreats();
+$(document).ready(function () {
+    function populateTreats() {
+        var treats = [];
+        $.get("/api/treats", function (data) {
+            for (i in data) {
+                if (treats.includes(data[i].candy) !== true) {
+                    $("#candyInput").append(
+                        $(
+                            "<option value='" +
+                            data[i].candy +
+                            "'>" +
+                            data[i].candy +
+                            "</option>"
+                        )
+                    );
+                    treats.push(data[i].candy);
+                }
+            }
+        });
+    }
+    populateTreats();
 
-  function populateNeighborhoods () {
-    $.get("/api/treats", function (data) {
-      var hoods = [];
-      for (i in data) {
-        if (hoods.includes(data[i].neighborhood) !== true) {
-          $("#neighborhoodInput").append(
-            $(
-              "<option value='" +
-                data[i].neighborhood +
-                "'>" +
-                data[i].neighborhood +
-                "</option>"
-            )
-          );
-          hoods.push(data[i].neighborhood);
-        }
-      }
-    });
-  }
+    function populateNeighborhoods() {
+        $.get("/api/treats", function (data) {
+            var hoods = [];
+            for (i in data) {
+                if (hoods.includes(data[i].neighborhood) !== true) {
+                    $("#neighborhoodInput").append(
+                        $(
+                            "<option value='" +
+                            data[i].neighborhood +
+                            "'>" +
+                            data[i].neighborhood +
+                            "</option>"
+                        )
+                    );
+                    hoods.push(data[i].neighborhood);
+                }
+            }
+        });
+    }
 
-  populateNeighborhoods();
+    populateNeighborhoods();
     // initializing variables
     $("#sub-btn-kids").on("click", function (event) {
         event.preventDefault();
@@ -59,19 +59,19 @@ $(document).ready(function() {
         cand = {
             candy: candySearch
         };
-        console.log(hood);
+        // console.log(hood);
 
         if ((neighborhoodSearch !== "nothing") && (candySearch === "nothing")) {
             $.get("/api/treats", function (data) {
-                console.log(data);
+                // console.log(data);
                 for (i in data) {
-                    console.log(data[i].neighborhood, hood.neighborhood);
+                    // console.log(data[i].neighborhood, hood.neighborhood);
 
                     if (data[i].neighborhood === hood.neighborhood) {
                         var selection = data[i].candy;
                         $("#candy-list").append($("<li>" + selection + "</li>"));
                         $("#addHere").html($('<h3 id="addHere">available candies</h3>'))
-                    } 
+                    }
                     // else {
                     //     $("#addHere").html("that neighborhood doesnt exist yet")
                     // }
@@ -80,15 +80,15 @@ $(document).ready(function() {
         }
         if ((candySearch !== "nothing") && (neighborhoodSearch === "nothing")) {
             $.get("/api/treats", function (data) {
-                console.log(data);
+                // console.log(data);
                 for (i in data) {
-                    console.log(data[i].candy, cand.candy);
+                    // console.log(data[i].candy, cand.candy);
 
                     if (data[i].candy === cand.candy) {
                         var selection = data[i].neighborhood;
                         $("#candy-list").append($("<li>" + selection + "</li>"));
                         $("#addHere").html($('<h3 id="addHere">find your candy in these neighborhoods</h3>'))
-                    } 
+                    }
                     // else {
                     //     $("#addHere").html("that candy doesnt exist yet")
                     // }
@@ -97,19 +97,25 @@ $(document).ready(function() {
         }
         if ((candySearch !== "nothing") && (neighborhoodSearch !== "nothing")) {
             $.get("/api/treats", function (data) {
-                console.log(data);
+                // console.log(data);
                 for (i in data) {
-                    if ((data[i].candy === cand.candy) && (data[i].neighborhood === hood.neighborhood) ) {
-                        var selection1 = data[i].candy;
-                        var selection2 = data[i].neighborhood;
-                        // $("#candy-list").append($("<li>" + selection + "</li>"));
-                        $("#addHere").html($('<h3 id="addHere">There are ' + candySearch + ' in ' + neighborhoodSearch + ' </h3>'))
-                    } 
-                    else if ((data[i].candy !== cand.candy) && (data[i].neighborhood === hood.neighborhood) ){
-                        $("#addHere").html($('<h3 id="addHere">There are not ' + candySearch + ' in ' + neighborhoodSearch + ' </h3>'))
+                    // console.log(data[i]);
+                    var bunch = [];
+                    if (data[i].neighborhood === hood.neighborhood) {
+                        bunch.push(data[i]);
+                        for (j in bunch) {
+                            console.log(bunch[j])
+                            if (bunch[j].candy === candySearch) {
+                                $("#addHere").html($('<h3 id="addHere">There are ' + candySearch + ' in ' + neighborhoodSearch + ' </h3>'))
+                            }
+                        }
+
                     }
-                    else if ((data[i].candy === cand.candy) && (data[i].neighborhood !== hood.neighborhood) ){
-                        $("#addHere").html($('<h3 id="addHere">There are not ' + candySearch + ' in ' + neighborhoodSearch + ' </h3>'))
+                    else if ((data[i].candy !== cand.candy) && (data[i].neighborhood === hood.neighborhood)) {
+                        $("#addHere").html($('<h3 id="addHere">There ain`t ' + candySearch + ' in ' + neighborhoodSearch + ' </h3>'))
+                    }
+                    else if ((data[i].candy === cand.candy) && (data[i].neighborhood !== hood.neighborhood)) {
+                        $("#addHere").html($('<h3 id="addHere">There ain`t ' + candySearch + ' in ' + neighborhoodSearch + ' </h3>'))
                     }
                 }
             });
